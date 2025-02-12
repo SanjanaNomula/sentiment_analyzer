bb3526e9b6c9e9f932ce68fab3efe144a4c75035
Sanjana Nomula <140239776+SanjanaNomula@users.noreply.github.com>
2025-02-13 01:13:51 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def main():
                 margin-right: 10px;
             }
             </style>
-            <a href="https://github.com/Hari-Narayana-Rath" class="github-button" target="_blank">
+            <a href="https://https://github.com/SanjanaNomula" class="github-button" target="_blank">
                 <i class="fab fa-github"></i> GitHub
             </a>
             """,
